132c40866aa311d81cec62037daa10c435e06022
blank71 <koblank18@gmail.com>
2019-11-15 22:50:28 +0900

    add config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+SITE_URL = 'https://mstdn.guru'
+
+# どれだけ過去のtootまで遡るか。 max_page_num * 40 toot分取得します
+max_page_num = 20
+
+# 高頻度語のうち上位いくつの単語を表示するか。
+top_num = 10
--- main.py
+++ main.py
@@ -1,46 +1,25 @@
-"""
-Fork form https://colab.research.google.com/drive/1kWvx8pIVPc6D6TyAwHWjk7nnZolX4VrU
-"""
-"""
-#@title
-!apt-get install -y -q sudo file mecab libmecab-dev mecab-ipadic-utf8 git curl python-mecab fonts-ipaexfont fonts-ipafont-gothic
-!git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
-!echo yes | mecab-ipadic-neologd/bin/install-mecab-ipadic-neologd -n
-!pip install -q natto-py neologdn
-!pip install -q mastodon.py beautifulsoup4
-!echo `mecab-config --dicdir`"/mecab-ipadic-neologd"
-"""
-"""## 設定をする
+# -*- coding:utf-8 -*-
 
-以下のセルでは、2つの設定パラメータがあります
-- `max_page_num`: 過去どれだけのtootまで遡るか。 1回 public timeline 40 toot取るので、40 * max_page_num 分のtootを取得します。
-- `top_num`: 単語を集計後、上位何位まで表示するかです。デフォルト50位まで表示します。
-"""
-
-#@title 設定項目
-# どれだけ過去のtootまで遡るか。 max_page_num * 40 toot分取得します
-max_page_num = 1 #@param {type:"slider", min:10, max:100, step:10}
-
-# 高頻度語のうち上位いくつの単語を表示するか。デフォルト50です。
-top_num = 10 #@param {type:"slider", min:10, max:100, step:1}
-
-"""## 以下はするーっと実行してください
-
-「ランタイム」→「以降のセルを実行」で何も考えずに実行してください。
-ただし、連打をするとAPI叩きまくってドリキンさんとさくらさんにご迷惑がかかるかもしれないので、常識の範囲内で...。
-"""
-
-#@title
+import config
 import requests
+import time
+from bs4 import BeautifulSoup
+from collections import Counter
+from natto import MeCab
+import neologdn
+import re
+import json
 
-api_path = "https://mstdn.guru/api/v1/timelines/public"
+max_page_num = config.max_page_num
+top_num = config.top_num
+SITE_URL = config.SITE_URL
+
+api_path = SITE_URL + "/api/v1/timelines/public"
 
 session = requests.Session()
 res = session.get(api_path, params={"local": True, "limit": 40})
 
-#@title
-import time
-from bs4 import BeautifulSoup
+
 
 toots = []
 
@@ -53,16 +32,7 @@ for i in range(0, max_page_num):
   res = session.get(next_link)
   time.sleep(1)
 
-#@title
 len(toots)
-
-#@title
-from collections import Counter
-from natto import MeCab
-import neologdn
-
-import re
-
 
 CONTENT_WORD_POS = ('名詞')
 
@@ -78,7 +48,7 @@ def extract_words(tagger, sentence):
     return words
 
 stop_words = {
-  "さん", "そう", "名前", "今日", "これ", "ここ", "きた", "やつ", "どこ", "www"
+  "さん", "そう", "名前", "今日", "これ", "ここ", "きた", "やつ", "どこ", "www", "自分", "記事"
 }
 
 stop_pattern = r"\d+"
@@ -97,12 +67,5 @@ for t in toots:
       continue
     cnt[w] += 1
 
-"""## 結果発表！！！
-
-以下が、過去800toot中、上位50位（デフォルト値）の名詞です。
-話題がなんとなくつかめましたか？
-"""
-
-#@title
 print(f"Top {top_num} words in past {max_page_num * 40} toots")
 print(cnt.most_common(top_num))
